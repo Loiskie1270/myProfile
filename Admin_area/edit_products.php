9c8eb99048ab6ad18a0407814ce425454a43c7e3
Loiskie1270 <130334150+Loiskie1270@users.noreply.github.com>
2023-08-23 19:27:49 +0800

    Add files via upload

--- Admin_area/edit_products.php
+++ Admin_area/edit_products.php
@@ -30,22 +30,31 @@ if(isset($_GET['edit_products'])){
      //echo  $brand_title;
 }
 ?>
-<div class="container mt-4">
-    <h3 class="text-center text-light">EDIT PRODUCT</h3>
-    <form action="" method="post" enctype="multipart/form-data">
-        <div class="form_outline w-50 m-auto mb-4">
-            <label for="product_title" class="form-label ">Product Title</label>
+<div class="container w-50" style="background-color:#ffbc7e;color:#fff; font-family: 'Poppins', sans-serif; font-size:15px;width:100%; display:block; margin-left:auto; margin-right:auto; border-radius:3px;">
+    <h3 class="text-center" style="background-color:#ff571d;color:#fff; font-family: 'Poppins', sans-serif; font-size:20px; padding:10px 10px 10px 10px; margin-bottom:15px; display: inline-block;  width:fit-content; margin-top:20px; border-radius:3px; margin-left:180px;" >EDIT PRODUCT</h3>
+
+    <form action="" method="post" enctype="multipart/form-data" style="background-color:#ffbc7e;color:#fff; width:100%;">
+
+        <div class="form_outline m-auto mb-0" style="width:80%; display:block; margin-left:auto; margin-right:auto;">
+
+            <label for="product_title" class="form-label">Product Title</label>
             <input type="text" id="product_title" value="<?php echo $product_title?>" name="product_title" class="form-control" required="required">
         </div>
-        <div class="form_outline w-50 m-auto mb-4">
+
+        <div class="form_outline m-auto mb-0" style="width:80%; display:block; margin-left:auto; margin-right:auto;">
+
             <label for="product_description" class="form-label ">Product Description</label>
             <input type="text" id="product_description" value="<?php echo $product_description?>" name="product_description" class="form-control" required="required">
         </div>
-        <div class="form_outline w-50 m-auto mb-4">
+
+        <div class="form_outline m-auto mb-0" style="width:80%; display:block; margin-left:auto; margin-right:auto;">
+
             <label for="product_keywords" class="form-label ">Product Keywords</label>
             <input type="text" id="product_keywords" value="<?php echo $product_keywords?>"  name="product_keywords" class="form-control" required="required">
         </div>
-        <div class="form_outline w-50 m-auto mb-4">
+
+        <div class="form_outline m-auto mb-0" style="width:80%; display:block; margin-left:auto; margin-right:auto;">
+
         <label for="product_category" class="form-label ">Product Category</label>
             <select name="product_category" class="form-select">
                 <option value="<?php echo $category_title?>"><?php echo $category_title?></option>
@@ -62,7 +71,7 @@ while($row_category_all=mysqli_fetch_assoc($result_category_all)){
                 
             </select>
         </div>
-        <div class="form_outline w-50 m-auto mb-4">
+        <div class="form_outline m-auto mb-0" style="width:80%; display:block; margin-left:auto; margin-right:auto;">
         <label for="product_brands" class="form-label ">Product Brand</label>    
             <select name="product_brands" class="form-select">
                 <option value="<?php echo $brand_title?>"><?php echo $brand_title?></option>
@@ -78,33 +87,33 @@ while($row_brand_all=mysqli_fetch_assoc($result_brand_all)){
                
             </select>
         </div>
-        <div class="form_outline w-50 m-auto mb-4">
-            <label for="product_image1" class="form-label">Product Image1</label>
+        <div class="form_outline m-auto mb-0" style="width:80%; display:block; margin-left:auto; margin-right:auto;margin-top:2px;">
+            <label for="product_image1" class="form-label">Image1</label>
             <div class="d-flex">
-            <input type="file" id="product_image1" name="product_image1" class="form-control w-60 m-auto" required="required">
-            <img src="./product_images/<?php echo $product_image1 ?>" alt="" class="product1_img">
+            <input type="file" id="product_image1" name="product_image1" class="form-control w-60 m-auto" required="required">&nbsp;&nbsp;
+            <img src="./product_images/<?php echo $product_image1 ?>" style="width:15%; border-radius:5px;" alt="" class="product1_img">
             </div>
         </div>
-        <div class="form_outline w-50 m-auto mb-4">
-            <label for="product_image2" class="form-label">Product Image2</label>
+        <div class="form_outline m-auto mb-0" style="width:80%; display:block; margin-left:auto; margin-right:auto;margin-top:-2px;">
+            <label for="product_image2" class="form-label" style="font-family: 'Poppins', sans-serif; font-size:14px; margin-top:-15px;">Image2</label>
             <div class="d-flex">
-            <input type="file" id="product_image2" name="product_image2" class="form-control w-60 m-auto" required="required">
-            <img src="./product_images/<?php echo $product_image2 ?>" alt="" class="product1_img">
+            <input type="file" id="product_image2" name="product_image2" class="form-control w-60 m-auto" required="required">&nbsp;&nbsp;
+            <img src="./product_images/<?php echo $product_image2 ?>" style="width:15%; border-radius:5px;" alt="" class="product1_img">
             </div>
         </div>
-        <div class="form_outline w-50 m-auto mb-4">
-            <label for="product_image3" class="form-label">Product Image3</label>
+        <div class="form_outline m-auto mb-0" style="width:80%; display:block; margin-left:auto; margin-right:auto; gap:2;">
+            <label for="product_image3" class="form-label" style="font-family: 'Poppins', sans-serif; font-size:14px; margin-top:-15px;">Image3</label>
             <div class="d-flex">
-            <input type="file" id="product_image3" name="product_image3" class="form-control w-60 m-auto" required="required">
-            <img src="./product_images/<?php echo $product_image3 ?>" alt="" class="product1_img">
+            <input type="file" id="product_image3" name="product_image3" class="form-control w-60 m-auto" required="required">&nbsp;&nbsp;
+            <img src="./product_images/<?php echo $product_image3 ?>" style="width:15%; border-radius:5px;" alt="" class="product1_img">
             </div>
         </div>
-        <div class="form_outline w-50 m-auto mb-4">
+        <div class="form_outline m-auto mb-4" style="width:80%; display:block; margin-left:auto; margin-right:auto;">
             <label for="product_price" class="form-label ">Product Price</label>
-            <input type="text" id="product_price" value="<?php echo $product_price?>" name="product_price" class="form-control" required="required">
+            <input type="text" id="product_price" value="<?php echo $product_price?>" name="product_price" class="form-control" required="required" style="margin-top:-10px;">
         </div>
         <div class="text-center">
-        <input type="submit" name="edit_product" value="Update Product" class="btn btn-info px-3 mb-3 border-0">
+        <input type="submit" name="edit_product" value="Update Product" style="background-color: #ff571d;color:#fff; margin-bottom:15px; display: inline-block; margin-left:50px; width: fit-content; margin-top:20px; border-radius:3px; margin-left:30px;"  class="btn mb-3 border-0">
         </div>
     </form>
 </div>
